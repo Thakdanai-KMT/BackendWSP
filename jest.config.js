3fbbe5064a7989378ba1b09da4ce3c6f98a2ec23
Fikkung <study0001fix@gmail.com>
2026-08-21 17:06:10 +0700

    chore: add TypeScript configuration file (tsconfig.json) with compiler options

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  collectCoverageFrom: ['src/**/*.ts'],
+};
