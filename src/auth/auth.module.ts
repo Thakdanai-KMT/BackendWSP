d6d1e0fafe1e8204c5d9f9705f8ab3552c8a20a7
thakdanai-k <study0001fix@gmail.com>
2026-09-24 13:46:36 +0700

    fix: update get_TOKEN script with correct email and password for authentication refactor: use forwardRef for UsersModule in AuthModule imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module,forwardRef } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
 import { AuthGuard } from './guards/auth.guard.js';
 import { UsersModule } from '../users/users.module.js';
 
 @Module({
-  imports: [UsersModule],
+  imports: [forwardRef(() => UsersModule)],
   controllers: [AuthController],
   providers: [AuthService, AuthGuard],
-  exports: [AuthService, AuthGuard, UsersModule], // เพิ่ม UsersModule ตรงนี้
+  exports: [AuthService, AuthGuard, UsersModule],
 })
 export class AuthModule {}
